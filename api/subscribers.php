bcdfbda901bc42d4773a36139f862c822743e9b6
kerenraf <kcs@kerencs.com>
2025-07-26 22:50:57 +0300

    Update subscribers.php

--- api/subscribers.php
+++ api/subscribers.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, DELETE');
